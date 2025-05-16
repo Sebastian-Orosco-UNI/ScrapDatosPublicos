83dfd7d308d02902ddaadc4a91abe3b2dcb9f5c0
Sebastian-Orosco-UNI <sebastian.orosco.m@uni.pe>
2025-05-15 19:25:51 -0500

    Update scrapersbs.py

--- scrapersbs.py
+++ scrapersbs.py
@@ -11,9 +11,11 @@ import os
 
 # Configuración del navegador
 options = Options()
-options.add_argument('--headless')  # Modo sin cabeza
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-blink-features=AutomationControlled')
+options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36')
 
 # Inicializar el driver usando webdriver-manager
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
